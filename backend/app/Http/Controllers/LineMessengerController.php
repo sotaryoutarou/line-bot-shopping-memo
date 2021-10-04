c45e271ad495a0535ae30ee15b0bbc4cb706cdf3
sotaryoutarou <sotaryoutarou@gmail.com>
2021-10-04 21:37:20 +0900

    add recods Memos from LINE

---
+++ backend/app/Http/Controllers/LineMessengerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Memo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class LineMessengerController extends Controller
+{
+    /**
+     * revieve line events
+     *
+     * @param  Request $request
+     * @return void
+     */
+    public function webhook(Request $request) {
+        foreach ($request->events as $event) {
+            if ($event['type'] === 'message') {
+                Memo::create([
+                    'content' => $event['message']['text'],
+                    'line_user_id' => $event['source']['userId']
+                ]);
+            }
+        }
+        
+    }
+
+    /**
+     * lineメッセージ受信用
+     *
+     * @param  Request $request
+     * @return void
+     */
+    public function message(Request $request) {
+
+    }
+}
