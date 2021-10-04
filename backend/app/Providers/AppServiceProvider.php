350f1c1a4e74074ddc7dcfa8fab1973d52cc6b74
sotaryoutarou <sotaryoutarou@gmail.com>
2021-10-04 22:12:53 +0900

    add line-bot facade

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // LINE BOT
+        $this->app->bind('line-bot', function ($app, array $parameters) {
+            return new LINEBot(
+                new CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN')),
+                ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]
+            );
+        });
     }
 
     /**
