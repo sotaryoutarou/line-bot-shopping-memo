21935af8588938533c6601d26587f4d95766462c
sotaryoutarou <sotaryoutarou@gmail.com>
2021-10-04 21:11:19 +0900

    make memos migration

---
+++ backend/database/migrations/2021_10_04_113649_create_memos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->id();
+            $table->string('line_user_id', 64)->unique();
+            $table->string('content', 128);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
