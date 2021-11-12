f595c71934a2f831b8505116fb1e2d53aac26afc
sotaryoutarou <sotaryoutarou@gmail.com>
2021-11-13 01:21:45 +0900

    fix style

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -6,17 +6,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.3/skins/all.min.css" integrity="sha512-wcKDxok85zB8F9HzgUwzzzPKJhHG7qMfC7bSKrZcFTC2wZXVhmgKNXYuid02cHVnFSC8KOJCXQ8M83UVA7v5Bw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.3/skins/flat/blue.min.css" integrity="sha512-NFzPiFD5sIrKyFzW9/n3DgL45vt0/5SL5KbQXsHyf63cQOXR5jjWBvU9mY3A80LOGPJSGApK8rNwk++RwZAS6Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
-
     <body>
-
-        <div class="container">
+        <div class="container mt-1">
             @yield('content')
         </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.3/icheck.min.js" integrity="sha512-RGDpUuNPNGV62jwbX1n/jNVUuK/z/GRbasvukyOim4R8gUEXSAjB4o0gBplhpO8Mv9rr7HNtGzV508Q1LBGsfA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.3/icheck.min.js" integrity="sha512-RGDpUuNPNGV62jwbX1n/jNVUuK/z/GRbasvukyOim4R8gUEXSAjB4o0gBplhpO8Mv9rr7HNtGzV508Q1LBGsfA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+        @yield('script')
     </body>
 </html>
--- backend/resources/views/memos/index.blade.php
+++ backend/resources/views/memos/index.blade.php
@@ -6,10 +6,31 @@
     @method('DELETE')
     @csrf
     @foreach ($memos as $memo)
-        <input type="checkbox" name="memos[]" value={{$memo->id}} id="memo_{{ $memo->id }}">
-        <label for="memo_{{ $memo->id }}">{{ $memo->content }}</label>
+      <div class="row pb-2 pt-2">
+        <label class="block">
+          <input type="checkbox" name="memos[]" value={{$memo->id}}>
+          <span class="label-text h1 align-bottom">{{ $memo->content }}</span>
+        </label>
+      </div>
     @endforeach
-    <input type="submit" value="完了">
+    <input type="submit" value="購入！" class="btn btn-info">
 </form>
 
+@endsection
+
+@section('script')
+<script>
+  $(document).ready(function(){
+    $('input').iCheck({  // inputタグのclass=icheckのものにだけ
+        checkboxClass: 'icheckbox_square-blue',
+        increaseArea: '40%' // optional
+    });
+  });
+  $('input').on('ifChecked', function(event){
+    $(this).parents('.row').css('background-color','#EEEEEE')
+  });
+  $('input').on('ifUnchecked', function(event){
+    $(this).parents('.row').css('background-color','')
+  });
+</script>
 @endsection
